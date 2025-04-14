e59f854857d6932c3882943c303dca6bb9f7be1d
haseeb7-Oz <Haseeb.Ali@followoz.com>
2025-04-14 22:27:45 +0500

    code refactor

---
+++ ProductManagementAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using ProductManagementAPI.Database;
+using ProductManagementAPI.Repositories.Interfaces;
+using ProductManagementAPI.Repositories;
+using ProductManagementAPI.Services.Interfaces;
+using ProductManagementAPI.Services;
+using System.Text;
+
+namespace ProductManagementAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IPlanManagementRepository, PlanManagementRepository>();
+            services.AddScoped<IPlanManagementService, PlanManagementService>();
+            services.AddScoped<ITierRepository, TierRepository>();
+            services.AddScoped<ITierService, TierService>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddSingleton<JwtService>();
+        }
+
+        public static void AddApplicationDb(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<ProductManagementDbContext>(options =>
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+        }
+
+        public static void AddCorsPolicy(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowReactApp", policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+        }
+
+        public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration config)
+        {
+            var jwtSettings = config.GetSection("JwtSettings");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]!);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.SaveToken = true;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = jwtSettings["Issuer"],
+                            ValidAudience = jwtSettings["Audience"],
+                            IssuerSigningKey = new SymmetricSecurityKey(key),
+                            ClockSkew = TimeSpan.Zero
+                        };
+                    });
+
+            services.AddAuthorization();
+        }
+
+        public static void AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter your JWT token in the format: Bearer {your_token}"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                    },
+                    new string[] { }
+                }
+            });
+            });
+        }
+
+        public static void ApplyDatabaseMigrations(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ProductManagementDbContext>();
+            dbContext.Database.Migrate();
+        }
+    }
+}
