ad4aba4d291446aa15361d687a62f6c7d08d5639
haseeb7-Oz <Haseeb.Ali@followoz.com>
2025-05-12 16:22:32 +0500

    Change Swagger Environment

--- ProductManagementAPI/Program.cs
+++ ProductManagementAPI/Program.cs
@@ -26,11 +26,8 @@ var app = builder.Build();
 //app.ApplyDatabaseMigrations();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseRouting();
